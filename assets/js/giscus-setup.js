fd5cd7f6773448afd783b3cb433e4cce961af8ca
Crista23 <garbacea@umich.edu>
2026-05-09 21:17:21 +0000

    Deploying to gh-pages from @ Crista23/Crista23.github.io@e5771af85ebdfa439acaefbeceb57891c73d5a4c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "Crista23/Crista23.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
